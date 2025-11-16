14d9952509fdfa9c1057ec349fce127b92ebd5d2
Mateus Rosario <mateusrosario.me@gmail.com>
2025-11-16 14:17:02 -0300

    refac: movendo providers para context (melhorando organização)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import LayoutContent from "@/components/LayoutContent";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-import { QueryProvider } from "@/providers/QueryProvider";
+import { QueryProvider } from "@/contexts/QueryProvider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
